63fb4a36654db116c97dcbb96b0abe5ecf3b1711
Ivan Klimov <klimov128@gmail.com>
2023-06-08 17:12:44 +0300

    Update App.styled.jsx

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -2,8 +2,7 @@ import styled from 'styled-components';
 
 export const AppContainer = styled.div`
   max-width: 600px;
-  margin: 0 auto;
-  margin-top: 40px;
+  margin: 40px auto;
   padding: 0 10px;
 
   & > h1 {
