b05f8e3562989ee152f89b162456a1360c23ccee
Ivan Klimov <klimov128@gmail.com>
2023-06-10 14:20:40 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,12 @@
 import { ContactListStyled } from './ContactList.styled';
-import { useDispatch, useSelector } from 'react-redux';
-import { getContacts, getIsLoading } from 'redux/contactListSlice';
+import { useSelector } from 'react-redux';
+import { getContacts } from 'redux/contactListSlice';
 import { getFilter } from 'redux/contactsFilterSlice';
-import { deleteContact } from 'redux/operations';
+import { Contact } from 'components/Contact';
 
 export const ContactList = () => {
-  const dispatch = useDispatch();
   const contacts = useSelector(getContacts);
   const filter = useSelector(getFilter);
-  const isLoading = useSelector(getIsLoading);
 
   const handleFilterContact = () => {
     const filterToLowerCase = filter.toLowerCase();
@@ -19,21 +17,11 @@ export const ContactList = () => {
   };
 
   const visibleContacts = handleFilterContact();
-  const handleDelete = id => dispatch(deleteContact(id));
 
   return (
     <ContactListStyled>
       {visibleContacts.map(contact => (
-        <li key={contact.id}>
-          {contact.name}:<span>{contact.number}</span>
-          <button
-            className={isLoading ? 'isLoading' : undefined}
-            type="button"
-            onClick={() => handleDelete(contact.id)}
-          >
-            delete
-          </button>
-        </li>
+        <Contact key={contact.id} contact={contact} />
       ))}
     </ContactListStyled>
   );
